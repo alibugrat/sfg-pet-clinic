d82894b1b07a357b27be46027b8867ea6238f2d2
Ali Buğra Topçuoğlu <alibugrat@gmail.com>
2021-10-28 16:50:42 +0300

    add bootstrap for data

--- pet-clinic-data/src/main/java/com/alibugrat/sfgpetclinic/services/map/PetServiceMap.java
+++ pet-clinic-data/src/main/java/com/alibugrat/sfgpetclinic/services/map/PetServiceMap.java
@@ -1,11 +1,11 @@
 package com.alibugrat.sfgpetclinic.services.map;
 
 import com.alibugrat.sfgpetclinic.model.Pet;
-import com.alibugrat.sfgpetclinic.services.CrudService;
+import com.alibugrat.sfgpetclinic.services.PetService;
 
 import java.util.Set;
 
-public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements PetService {
     @Override
     public Set<Pet> findAll() {
         return super.findAll();
--- pet-clinic-data/src/main/java/com/alibugrat/sfgpetclinic/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/com/alibugrat/sfgpetclinic/services/map/VetServiceMap.java
@@ -1,11 +1,11 @@
 package com.alibugrat.sfgpetclinic.services.map;
 
 import com.alibugrat.sfgpetclinic.model.Vet;
-import com.alibugrat.sfgpetclinic.services.CrudService;
+import com.alibugrat.sfgpetclinic.services.VetService;
 
 import java.util.Set;
 
-public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
     @Override
     public Set<Vet> findAll() {
         return super.findAll();

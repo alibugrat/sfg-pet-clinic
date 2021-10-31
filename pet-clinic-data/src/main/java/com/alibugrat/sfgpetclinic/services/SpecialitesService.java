c4e10e6d4d78d421f2aaae3ed725d4fb0723ef88
Ali Buğra Topçuoğlu <alibugrat@gmail.com>
2021-10-31 13:12:01 +0300

    add Specialities Map Impl

---
+++ pet-clinic-data/src/main/java/com/alibugrat/sfgpetclinic/services/SpecialitesService.java
@@ -0,0 +1,6 @@
+package com.alibugrat.sfgpetclinic.services;
+
+import com.alibugrat.sfgpetclinic.model.Speciality;
+
+public interface SpecialitesService extends  CrudService<Speciality, Long> {
+}
